9bdcd0a6a5c10291eae6c206d0126488718cc3b9
Fortegrans <titaniumzed@gmail.com>
2026-06-05 14:14:02 +0300

    Cumartesi Pazar ve aksam saatleri eklendi

--- src/components/DersFormu.jsx
+++ src/components/DersFormu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-const GUNLER = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma']
-const SAATLER = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+const GUNLER = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
+const SAATLER = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
 
 function DersFormu({ onDersEkle }) {
   const [dersAdi, setDersAdi] = useState('')
